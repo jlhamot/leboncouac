e8a8dad89f63b3a559d54af101c478754179525f
Jean-Luc Hamot <jlhamot@free.fr>
2011-12-20 17:36:39 +0100

    first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leboncouac::Application.config.secret_token = '3d2ffd7df3c4e3e138ca6d96264d5e01cba17d25a5c12787f19bf108433b869bfc6351e04cc62cef4a568ddb57e51ce4d2f3351f9a034aef344ac010ff87a2c8'
