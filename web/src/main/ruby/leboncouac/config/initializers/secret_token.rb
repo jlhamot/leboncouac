d51b3acd35b691648a42492b5e7cdb3590d9a713
Jean-Luc Hamot <jlhamot@free.fr>
2011-12-21 14:38:15 +0100

    AMA deleted

--- web/src/main/ruby/leboncouac/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Leboncouac::Application.config.secret_token = '853de5b190f852179d18024fde4d025d1718e6c59f5780802637a3744a3253795215be67f25575a37012da24fa058f274eb3916f8afe21b89c4e670c1a2569dd'
