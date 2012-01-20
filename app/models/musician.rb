a57fbf3f45d635a0cf567c4915fe976b1f447aab
Jean-Luc Hamot <jlhamot@free.fr>
2012-01-20 11:44:34 +0100

    Modification require securerandom

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require 'SecureRandom'
+require 'securerandom'
 
 class Musician < ActiveRecord::Base
   
--- db/migrate/20120117150234_add_uuid_to_musician.rb
+++ db/migrate/20120117150234_add_uuid_to_musician.rb
@@ -1,5 +1,3 @@
-require 'SecureRandom'
-
 class AddUuidToMusician < ActiveRecord::Migration
   def change
     add_column :musicians, :uuid, :string
